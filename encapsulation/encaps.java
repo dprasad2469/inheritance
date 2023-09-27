d86c37537b4b87e7d994231f567b130ecbafcd14
dprasad2469 <dprasad2469@gmail.com>
2023-09-27 10:26:19 -0700

    new

---
+++ encapsulation/encaps.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class encaps {
+	private final String val = "Durga prasad";
+	private int val2 = 28;
+	
+	public String getval() {
+		return val;
+	}
+	public int getval2() {
+		return val2;
+	}
+}
---
+++ encapsulation/main.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class main extends encaps {
+
+	public static void main(String[] args) {
+		main caps = new main();
+		String val = caps.getval();
+		System.out.println(val);
+		System.out.println(caps.getval2());
+	}
+
+}
